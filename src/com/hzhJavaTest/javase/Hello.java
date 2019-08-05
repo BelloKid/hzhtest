bacddfa1cfa5b68e2802fb32426585941021da0c
hzh <zxas5548@126.com>
2019-08-05 10:18:02 +0800

    init HelloWorld

---
+++ src/com/hzhJavaTest/javase/Hello.java
@@ -0,0 +1,24 @@
+package com.hzhJavaTest.javase;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a=1;
+        int b=a+2;
+
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        aa();
+        System.out.println("Hello World");
+    }
+
+
+
+    public static void aa() {
+        System.out.println("Hello Java");
+    }
+}
+
+
