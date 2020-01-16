24a9b47e160fc88d3ab594cc54732f50af15d950
hzh <zxas5548@126.com>
2020-01-16 09:31:37 +0800

    init HelloWorld

---
+++ src/com/hzhJavaTest/javase/Welcome.java
@@ -0,0 +1,13 @@
+package com.hzhJavaTest.javase;
+
+public class Welcome {
+    public static void main (String[] args)
+        {
+        String greeting ="Welcome core java";
+        System.out.println(greeting);
+        for(int i=0;i < greeting.length();i++)
+            System.out.println("=");
+        System.out.println(
+        );
+    }
+}
